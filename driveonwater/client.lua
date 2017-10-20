3141e724f89953c59941c7f65b03681f7f478a8b
IllusiveTea <31740751+IllusiveTea@users.noreply.github.com>
2017-10-20 21:56:11 +0100

    Added /waterwalk

--- driveonwater/client.lua
+++ driveonwater/client.lua
@@ -12,6 +12,20 @@ RegisterCommand("driveonwater", function(source, args, rawCommand)
     end
 end,false)
 
+local toggle2 = false 
+local walkw = false
+RegisterCommand("waterwalk", function(source, args, rawCommand)
+    toggle2 = not toggle2
+    if toggle2 then
+        walkw = true
+        TriggerEvent("chatMessage", "", {255,255,255}, "Walk On Water: ^2^*Enabled")
+
+    else
+        walkw = false
+        TriggerEvent("chatMessage", "", {255,255,255}, "Walk On Water: ^8^*Disabled")
+    end
+end,false)
+
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
@@ -24,6 +38,23 @@ Citizen.CreateThread(function()
             SetEntityDynamic(object, false)
             SetEntityRotation(object, 0.0, 180.0, -hed, 1, true)
             Citizen.Wait(35)
+            DeleteEntity(object)    
+        end
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        local x, y, z = table.unpack(GetEntityCoords(PlayerPedId(), true))
+        gotheight, height = GetWaterHeight(x, y, z, z)
+        if IsPedOnFoot(PlayerPedId()) and gotheight and walkw then
+            hed = GetEntityHeading(PlayerPedId())
+            local object = CreateObject(GetHashKey("prop_lev_des_barge_01"), x, y, height-4.0, true, false, false)
+            SetEntityAlpha(object, 0, true)
+            SetEntityDynamic(object, false)
+            SetEntityRotation(object, 0.0, 180.0, -hed, 1, true)
+            Citizen.Wait(100)
             DeleteEntity(object)    
         end
     end
